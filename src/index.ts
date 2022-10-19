3c831ec3879de0c17501609b82640104ad7b9325
lucasjstn <lucas7justino@gmail.com>
2022-10-19 13:24:42 -0300

    general example of generics usage

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,19 @@
 //criar uma interface
 
-interface UserInterface {
+//sintaxe =
+//<"LEtra">, ex: <T>, <G>
+//depois na variavel da interface por a letra que utilizou, se usou <T> usar o T;
+//depois do tipo do objeto criado a partir da interface usar <tipo>, ex: <string>
+//na atribuição do objeto precisasse coincidir com o tipo, se nao vai dar erro   TS2322: Type 'string[]' is not assignable to type 'number'.
+interface UserInterface<T> {
     name: string,
     age: number,
-    data: string[]
+    data: T,
 }
 
 //criar um objeto
 
-const person: UserInterface = {
+const person: UserInterface<number> = {
     name: "ALexandre",
     age: 39,
     data: ['alexandre', 'tete', 'dificil']
