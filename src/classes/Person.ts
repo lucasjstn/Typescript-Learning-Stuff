b2cb34d467b17a147485b196063705ff0241cabc
lucasjstn <lucas7justino@gmail.com>
2022-10-19 12:47:05 -0300

    static property examplee

---
+++ src/classes/Person.ts
@@ -0,0 +1,7 @@
+export class Person {
+    static myName: string = "Aluisio da Cunha"
+
+    info(): string {
+        return Person.myName;
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
-import { name } from "./teste";
+import { Person } from "./classes/Person";
+//atributos e metodos estaticos podem ser acessados sem precisar instanciar
 
-console.log(name);
+// const p = new Person()
+// console.log(p.myName); //Property 'myName' does not exist on type 'Person'. Did you mean to access the static member 'Person.myName' instead
+
+Person.myName = 'asdasd';
+
+const p = new Person();
+
+console.log(p.info());
