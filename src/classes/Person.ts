ef8737f7c3769c83da988d05afec695829c05a58
lucasjstn <lucas7justino@gmail.com>
2022-10-19 04:14:53 -0300

    best way to use constructor

--- src/classes/Person.ts
+++ src/classes/Person.ts
@@ -1,11 +1,6 @@
 export class Person {
-    name: string;
-    age: number | string;
+    constructor(public name: string, public age: number | string) { }
 
-    constructor(name: string, age: number | string) {
-        this.name = name;
-        this.age = age;
-    }
 
     info(): string {
         return `Meu nome é ${this.name}, e minha idade é de ${this.age}`
