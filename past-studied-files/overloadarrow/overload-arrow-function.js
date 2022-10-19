295ef3dfd79511b68c03fd4b45b376e83994940a
lucasjstn <lucas7justino@gmail.com>
2022-10-18 21:27:01 -0300

    optional values

--- past-studied-files/overloadarrow/overload-arrow-function.js
+++
@@ -1,6 +0,0 @@
-let person;
-//se nao tiver esperando nada usar void
-person = (name, age) => {
-    return age;
-};
-console.log(person("Lucas", 232123));
---
+++ src/optional-parameters-and-properties.ts
@@ -0,0 +1,36 @@
+/** let user: {
+    name: string,
+    age: number;
+    color?: string;
+    isAdmin?: boolean,
+}
+
+nome e interrogação faz a propriedade ser opcional
+
+user = {
+    name: 'lucas',
+    age: 23,
+    color: 'blue'
+}
+
+console.log(user); **/
+
+//=============================
+
+//DA PRA FAZER O MESMO COM FUNÇÕES
+
+// function person(name: string, age: number, color: string, isAdmin: boolean) {
+
+// }
+
+// person('lucas', 23, true)
+// Expected 4 arguments, but got 3.
+// A required parameter cannot follow an optional parameter.
+//os parametros opcionais precisam estar nas ultimas colocações
+
+function person(name: string, age: number, isAdmin: boolean, color?: string) {
+
+}
+
+person('lucas', 23, true)
+
