2c9162d040ebaf98561186dbe72e0fc3ef8dc2f6
lucasjstn <lucas7justino@gmail.com>
2022-10-21 05:56:15 -0300

    solving conflit with cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,15 @@
 import express from "express";
-import { index as homeIndex } from "../controllers/Home";
-
+import cors from "cors";
 //routes
 import home from "../routes/home";
+import user from "../routes/user";
 
 const app = express();
 
+app.use(cors());
+
 app.use("/", home);
+app.use("/user", user);
 
 app.listen(3000, function () {
   console.log("servidor rodando ");
